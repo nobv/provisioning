f0586c11ab3460a614698d388345123fe9b14601
Nobv <6e6f6275@gmail.com>
2018-09-12 23:42:06 +0900

    [wip] introducing linuxbrew...

--- download.sh
+++ download.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+daclare PLATFORM
+daclare HOMEBREW
+
 readonly PROVISIONINGPATH=~/.osx-provisioning
 readonly DOTPATH=~/.dotfiles
 readonly DOWNLOADSPATH=~/Downloads
@@ -10,13 +13,58 @@ is_exsits() {
   return $?
 }
 
+os_type() {
+  uname | tr "[:upper:]" "[:lower:]"
+}
+
+os_judgment() {
+  case "$(ostype)" in
+    *'linux'*)
+      PLATFORM='linux'
+      HOMEBREW='sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"'
+      ;;
+    *'darwin'*)
+      PLATFORM='osx'
+      HOMEBREW='/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+      ;;
+    *'bsd'*)
+      PLATFORM='bsd'
+      HOMEBREW=''
+      ;;
+    *)
+      PLATFORM='unknown'
+      HOMEBREW=''
+      ;;
+  esac
+  export PLATFORM
+}
+
+is_osx() {
+  os_detect
+  if [ "$PLATFORM" = "osx" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+is_linux() {
+  os_detect
+  if [ "$PLATFORM" = "linux" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+os_judgment
+
 if ! is_exsits "brew"; then
   echo "******************************"
   echo "Start installing Homebrew!!!"
   echo "******************************"
-  
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  
+
+  $(HOMEBREW)
   brew update
   brew upgrade --all --cleanup
   brew -v
