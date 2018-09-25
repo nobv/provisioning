fb13e19c43d1f9177af6fc4c31cfeffc2a022a09
Nobv <6e6f6275@gmail.com>
2018-09-26 01:57:03 +0900

    :art: move download.sh to initialize.sh

---
+++ initialize.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+daclare PLATFORM
+daclare HOMEBREW
+
+readonly PROVISIONINGPATH=~/.osx-provisioning
+readonly DOTPATH=~/.dotfiles
+
+is_exsits() {
+  which "$1" >/dev/null 2>&1
+  return $?
+}
+
+os_type() {
+  uname | tr "[:upper:]" "[:lower:]"
+}
+
+os_judgment() {
+  case "$(ostype)" in
+    *'linux'*)
+      PLATFORM='linux'
+      HOMEBREW='sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"'
+      ;;
+    *'darwin'*)
+      PLATFORM='osx'
+      HOMEBREW='/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+      ;;
+    *'bsd'*)
+      PLATFORM='bsd'
+      HOMEBREW=''
+      ;;
+    *)
+      PLATFORM='unknown'
+      HOMEBREW=''
+      ;;
+  esac
+  export PLATFORM
+}
+
+is_osx() {
+  os_judgment
+  if [ "$PLATFORM" = "osx" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+is_linux() {
+  os_judgment
+  if [ "$PLATFORM" = "linux" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+os_judgment
+
+if ! is_exsits "brew"; then
+  echo "******************************"
+  echo "Start installing Homebrew!!!"
+  echo "******************************"
+
+  $(HOMEBREW)
+  brew update
+  brew upgrade --all --cleanup
+  brew -v
+
+  echo "******************************"
+  echo "Finished!!!"
+  echo "******************************"
+
+fi
+
+if [ ! -d "${PROVISIONINGPATH}" ]; then
+  echo "******************************"
+  echo "Start installing osx-provisioning.git!!!"
+  echo "******************************"
+  git clone https://github.com/Nobv/osx-provisioning.git 
+  echo "******************************"
+  echo "Finished!!!"
+  echo "******************************"
+fi
+
+if [ ! -d "${DOTPATH}" ]; then
+  echo "Downloading dotfiles..."
+  cd ${HOME}
+  echo ${PWD}
+  #bash -c "$(curl -L https://github.com/Nobv/dotfiles.git)"
+  git clone https://github.com/Nobv/dotfiles.git .dotfiles
+  cd .dotfiles
+  sh install.sh
+fi
+
