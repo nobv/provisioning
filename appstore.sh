c0cc182f8f75c1ce3e0b14aef1ee6733dba9f852
Nobv <36393714+Nobv@users.noreply.github.com>
2019-01-18 12:27:14 +0900

    Update

--- appstore.sh
+++ appstore.sh
@@ -8,7 +8,7 @@ is_mas_exists() {
 }
 
 if ! is_mas_exists "Airmail 3"; then
-  mas install 918858936
+  #mas install 918858936
 fi
 
 if ! is_mas_exists "Slack"; then
@@ -16,37 +16,37 @@ if ! is_mas_exists "Slack"; then
 fi
 
 if ! is_mas_exists "LINE"; then
-  mas install 539883307
+  #mas install 539883307
 fi
 
 if ! is_mas_exists "The Unarchiver"; then
-  mas install 425424353
+  #mas install 425424353
 fi
 
 if ! is_mas_exists "Amphetamine"; then
-  mas install 937984704
+  #mas install 937984704
 fi
 
 if ! is_mas_exists "Magnet"; then
-  mas install 441258766
+  #mas install 441258766
 fi
 
 if ! is_mas_exists "Alfred"; then
-  mas install 405843582
+  #mas install 405843582
 fi
 
 if ! is_mas_exists "Day One"; then
-  mas install 1055511498
+  #mas install 1055511498
 fi
 
 if ! is_mas_exists "YubiKey Personalization Tool"; then
-  mas install 638161122
+  #mas install 638161122
 fi
 
 if ! is_mas_exists "Disk Diag"; then
-  mas install 672206759
+  #mas install 672206759
 fi
 
 if ! is_mas_exists "iThoughtsX"; then
-  mas install 720669838
+  #mas install 720669838
 fi
